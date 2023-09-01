88d6bd6f57f1d5dd80786f9b68a90cfa130dc336
Jackie <1824561218@qq.com>
2023-09-01 18:24:03 +0800

    fix

--- src/views/WalletConnect.vue
+++ src/views/WalletConnect.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Jackie
  * @Date: 2023-09-01 15:05:56
- * @LastEditTime: 2023-09-01 18:04:49
+ * @LastEditTime: 2023-09-01 18:19:51
  * @LastEditors: Jackie
  * @Description: file content
  * @FilePath: /Vue3-Wallet-demo/src/views/WalletConnect.vue
@@ -10,11 +10,9 @@
 <template>
   <div class="WalletConnect">
     <h1>This is an WalletConnect page-{{ ID }}</h1>
-    <button>Walletconnect</button>
   </div>
 </template>
 <script setup>
 import { onMounted, onUnmounted } from 'vue';
 const ID = process.env.VUE_APP_PROJECTID;
-onMounted(() => {});
 </script>
