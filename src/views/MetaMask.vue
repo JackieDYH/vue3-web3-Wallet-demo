d177f1b80c6c213b24c6dab57aeef08c39aa7a22
Jackie <1824561218@qq.com>
2023-09-01 16:39:23 +0800

    metamask连接和签名

--- src/views/MetaMask.vue
+++ src/views/MetaMask.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Jackie
  * @Date: 2023-06-25 09:58:10
- * @LastEditTime: 2023-09-01 15:22:17
+ * @LastEditTime: 2023-09-01 16:38:55
  * @LastEditors: Jackie
  * @Description: file content
  * @FilePath: /Vue3-Wallet-demo/src/views/MetaMask.vue
@@ -10,34 +10,64 @@
 <template>
   <div class="MetaMask">
     <h1>This is an MetaMask page</h1>
+    <button @click="getAddress">获取用户地址</button>
     <button @click="connectMetaMask">连接MetaMask</button>
     <button @click="sendTransaction">发送交易</button>
   </div>
 </template>
 <script setup>
-// 导入ethers.js库，并创建一个ethers.providers.Web3Provider实例
-// import { ethers } from 'ethers';
-// const provider = new ethers.providers.Web3Provider(window.ethereum);
+import { onMounted, onUnmounted } from 'vue';
+import { ethers } from 'ethers';
+console.log('ethers', ethers);
 
-// 在用户点击登录按钮或者触发登录事件时，调用provider实例的getSigner()方法获取用户的签名对象。
-const connectMetaMask = async () => {
-  try {
-    // eth_requestAccounts是MetaMask的请求方法，用于请求用户授权连接应用程序
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const signer = provider.getSigner();
-    // 签名对象可用于执行以太坊交易或其他操作
-    console.log('已连接到MetaMask', signer);
-  } catch (error) {
-    console.error('连接MetaMask失败', error);
+// 获取用户钱包地址
+const getAddress = async () => {
+  // 一
+  // const provider = window.ethereum;
+  // if (typeof provider == 'undefined') {
+  //   console.log('Please install MetaMask');
+  //   return;
+  // }
+  // console.log('provider', provider);
+  // const address = await provider.request({ method: 'eth_requestAccounts' });
+  // console.log('address', address);
+  // 获取签名
+  // const signer = await provider.request({
+  //   method: 'personal_sign',
+  //   params: [address[0], '0x12323131231231231232']
+  // });
+  // console.log('signer', signer);
+  // 二
+  let signer = null;
+  let provider;
+  if (window.ethereum == null) {
+    console.log('MetaMask not installed; using read-only defaults');
+    provider = ethers.getDefaultProvider();
+  } else {
+    provider = new ethers.BrowserProvider(window.ethereum);
+    // 获取地址
+    signer = await provider.getSigner();
+    // 获取签名
+    let s = await signer.signMessage(
+      'You are signing into abcdtest.link trading 003101'
+    );
+    console.log(s);
   }
+  console.log('signer', signer.address, provider);
 };
 
-// 使用signer对象来执行以太坊交易、查询账户信息等
-const sendTransaction = async () => {
-  const transaction = await signer.sendTransaction({
-    to: '0x1234567890abcdef...',
-    value: ethers.utils.parseEther('1.0')
+onMounted(() => {
+  window.ethereum.on('accountsChanged', (accounts) => {
+    // 处理账户变更事件
+    console.log('账户地址已变更', accounts);
   });
-  console.log('交易已发送', transaction);
-};
+});
+
+onUnmounted(() => {
+  console.log('销毁');
+  window.ethereum.removeListener('accountsChanged', (accounts) => {
+    // 处理账户变更事件
+    console.log('账户地址已变更-2', accounts);
+  });
+});
 </script>
