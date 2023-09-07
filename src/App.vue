a24afb31064da28fed291584416d3aa785c7bfbb
Jackie <1824561218@qq.com>
2023-09-07 19:58:11 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Jackie
  * @Date: 2023-06-25 09:58:10
- * @LastEditTime: 2023-09-07 19:57:31
+ * @LastEditTime: 2023-09-07 19:58:02
  * @LastEditors: Jackie
  * @Description: file content
  * @FilePath: /Vue3-Wallet-demo/src/App.vue
